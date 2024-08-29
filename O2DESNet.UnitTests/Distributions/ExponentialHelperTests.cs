ba32c92ef929726ffb01f621195eb4eae29d63fc
goh-chunlin <chunlin@live.com.sg>
2024-08-29 15:33:54 +0800

    Refactored ExponentialHelper UT.

--- O2DESNet.UnitTests/Distributions/ExponentialHelperTests.cs
+++ O2DESNet.UnitTests/Distributions/ExponentialHelperTests.cs
@@ -1,67 +1,70 @@
 ﻿using NUnit.Framework;
 using O2DESNet.Distributions;
 
-namespace O2DESNet.UnitTests.Distributions
+namespace O2DESNet.UnitTests.Distributions;
+
+[TestFixture]
+internal class ExponentialHelperTests
 {
-    [TestFixture]
-    internal class ExponentialHelperTests
+    private const double TolerenceAmountForEqual = 0.0000001;
+    
+    [Test]
+    public void Sample_GivenPositiveMean_ReturnsPositiveTimeSpan()
     {
-        [Test]
-        public void Sample_GivenPositiveMean_ReturnsPositiveTimeSpan()
-        {
-            var mean = TimeSpan.FromSeconds(5);
-            var result = ExponentialHelper.Sample(new Random(100), mean, TimeUnit.Seconds);
+        var result = ExponentialHelper.Sample(new Random(100), TimeSpan.FromSeconds(5), TimeUnit.Seconds);
 
-            Assert.That(result.TotalSeconds, Is.GreaterThan(0), "Sample should return a positive TimeSpan");
-        }
+        Assert.That(result.TotalSeconds, Is.GreaterThan(0), "Sample should return a positive TimeSpan");
+    }
 
-        [Test]
-        public void Sample_WhenMeanIsZeroOrNegative_ThrowsArgumentOutOfRangeException()
-        {
-            var mean = TimeSpan.Zero;
-            Assert.Throws<ArgumentOutOfRangeException>(() => ExponentialHelper.Sample(new Random(100), mean, TimeUnit.Seconds));
-        }
+    [Test]
+    public void Sample_WhenMeanIsZeroOrNegative_ThrowsArgumentOutOfRangeException()
+    {
+        var mean = TimeSpan.Zero;
+        
+        Assert.Throws<ArgumentOutOfRangeException>(() => ExponentialHelper.Sample(new Random(100), mean, TimeUnit.Seconds));
+    }
 
-        [Test]
-        public void CDF_GivenValidInputs_ReturnsExpectedValue()
-        {
-            double mean = 5.0;
-            double x = 2.0;
-            double result = ExponentialHelper.CDF(mean, x);
+    [Test]
+    public void CDF_GivenValidInputs_ReturnsExpectedValue()
+    {
+        const double mean = 5.0;
+        const double x = 2.0;
+        
+        var result = ExponentialHelper.Cdf(mean, x);
 
-            Assert.That(result, Is.EqualTo(0.3296799539643607).Within(0.0000001));
-        }
+        Assert.That(result, Is.EqualTo(0.3296799539643607).Within(TolerenceAmountForEqual));
+    }
 
-        [Test]
-        public void InvCDF_GivenValidInputs_ReturnsExpectedValue()
-        {
-            double mean = 5.0;
-            double p = 0.5;
-            double result = ExponentialHelper.InvCDF(mean, p);
+    [Test]
+    public void InvCDF_GivenValidInputs_ReturnsExpectedValue()
+    {
+        const double mean = 5.0;
+        const double p = 0.5;
+        
+        var result = ExponentialHelper.InvCdf(mean, p);
 
-            Assert.That(result, Is.EqualTo(3.4657359027997265).Within(0.0000001));
-        }
+        Assert.That(result, Is.EqualTo(3.4657359027997265).Within(TolerenceAmountForEqual));
+    }
 
-        [Test]
-        public void Sample_GivenSmallTimeSpan_DoesNotCausePrecisionIssues()
-        {
-            var mean = TimeSpan.FromMilliseconds(1);
-            var result = ExponentialHelper.Sample(new Random(100), mean, TimeUnit.Seconds);
+    [Test]
+    public void Sample_GivenSmallTimeSpan_DoesNotCausePrecisionIssues()
+    {
+        var mean = TimeSpan.FromMilliseconds(1);
+        var result = ExponentialHelper.Sample(new Random(100), mean, TimeUnit.Seconds);
 
-            Assert.That(result.TotalMilliseconds, Is.GreaterThan(0), "Sample should handle very small time spans without precision issues");
-        }
+        Assert.That(result.TotalMilliseconds, Is.GreaterThan(0), "Sample should handle very small time spans without precision issues");
+    }
 
-        [Test]
-        public void Sample_WithVariousTimeUnits_ReturnsConsistentResults()
-        {
-            var resultInSeconds = ExponentialHelper.Sample(new Random(100), TimeSpan.FromSeconds(3600), TimeUnit.Seconds).TotalSeconds;
-            var resultInMinutes = ExponentialHelper.Sample(new Random(100), TimeSpan.FromMinutes(60), TimeUnit.Minutes).TotalSeconds;
-            var resultInHours = ExponentialHelper.Sample(new Random(100), TimeSpan.FromHours(1), TimeUnit.Hours).TotalSeconds;
-            
-            // Since the random seed and mean are the same, the results should be consistent
-            Assert.That(resultInSeconds, Is.EqualTo(resultInHours).Within(0.00001));
-            Assert.That(resultInMinutes, Is.EqualTo(resultInHours).Within(0.00001));
-            Assert.That(resultInSeconds, Is.EqualTo(resultInMinutes).Within(0.00001));
-        }
+    [Test]
+    public void Sample_WithVariousTimeUnits_ReturnsConsistentResults()
+    {
+        var resultInSeconds = ExponentialHelper.Sample(new Random(100), TimeSpan.FromSeconds(3600), TimeUnit.Seconds).TotalSeconds;
+        var resultInMinutes = ExponentialHelper.Sample(new Random(100), TimeSpan.FromMinutes(60), TimeUnit.Minutes).TotalSeconds;
+        var resultInHours = ExponentialHelper.Sample(new Random(100), TimeSpan.FromHours(1), TimeUnit.Hours).TotalSeconds;
+        
+        // Since the random seed and mean are the same, the results should be consistent
+        Assert.That(resultInSeconds, Is.EqualTo(resultInHours).Within(TolerenceAmountForEqual));
+        Assert.That(resultInMinutes, Is.EqualTo(resultInHours).Within(TolerenceAmountForEqual));
+        Assert.That(resultInSeconds, Is.EqualTo(resultInMinutes).Within(TolerenceAmountForEqual));
     }
 }
--- O2DESNet/Distributions/ExponentialHelper.cs
+++ O2DESNet/Distributions/ExponentialHelper.cs
@@ -1,87 +1,84 @@
-﻿using System;
+﻿namespace O2DESNet.Distributions;
 
-namespace O2DESNet.Distributions
+/// <summary>
+/// Provides methods for sampling from an exponential distribution.
+/// </summary>
+public static class ExponentialHelper
 {
     /// <summary>
-    /// Provides methods for sampling from an exponential distribution.
+    /// Samples a value from an exponential distribution given a mean.
     /// </summary>
-    public static class ExponentialHelper
+    /// <param name="rs">The random number generator.</param>
+    /// <param name="mean">The mean of the exponential distribution.</param>
+    /// <returns>The sampled value.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean is less than or equal to zero.</exception>
+    public static double Sample(Random rs, double mean)
     {
-        /// <summary>
-        /// Samples a value from an exponential distribution given a mean.
-        /// </summary>
-        /// <param name="rs">The random number generator.</param>
-        /// <param name="mean">The mean of the exponential distribution.</param>
-        /// <returns>The sampled value.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean is less than or equal to zero.</exception>
-        public static double Sample(Random rs, double mean)
+        ValidateMean(mean);
+
+        return MathNet.Numerics.Distributions.Exponential.Sample(rs, 1 / mean);
+    }
+
+    /// <summary>
+    /// Samples a time span value from an exponential distribution given a mean time span.
+    /// </summary>
+    /// <param name="rs">The random number generator.</param>
+    /// <param name="mean">The mean time span of the exponential distribution.</param>
+    /// <param name="timeUnit">The unit of the TimeSpan to return.</param>
+    /// <returns>The sampled time span in the specified unit.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean time span is less than or equal to zero or if an invalid unit is provided..</exception>
+    public static TimeSpan Sample(Random rs, TimeSpan mean, TimeUnit timeUnit)
+    {
+        return timeUnit switch
         {
-            ValidateMean(mean);
+            TimeUnit.Days => TimeSpan.FromDays(Sample(rs, mean.TotalDays)),
+            TimeUnit.Hours => TimeSpan.FromHours(Sample(rs, mean.TotalHours)),
+            TimeUnit.Minutes => TimeSpan.FromMinutes(Sample(rs, mean.TotalMinutes)),
+            TimeUnit.Seconds => TimeSpan.FromSeconds(Sample(rs, mean.TotalSeconds)),
+            TimeUnit.Milliseconds => TimeSpan.FromMilliseconds(Sample(rs, mean.TotalMilliseconds)),
+            _ => throw new ArgumentOutOfRangeException(nameof(timeUnit), "Unsupported time unit.")
+        };
+    }
 
-            return MathNet.Numerics.Distributions.Exponential.Sample(rs, 1 / mean);
-        }
+    /// <summary>
+    /// Computes the cumulative distribution function (CDF) for a given value.
+    /// </summary>
+    /// <param name="mean">The mean of the exponential distribution.</param>
+    /// <param name="x">The value for which the CDF is computed.</param>
+    /// <returns>The CDF value.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean is less than or equal to zero.</exception>
+    public static double Cdf(double mean, double x)
+    {
+        ValidateMean(mean);
 
-        /// <summary>
-        /// Samples a time span value from an exponential distribution given a mean time span.
-        /// </summary>
-        /// <param name="rs">The random number generator.</param>
-        /// <param name="mean">The mean time span of the exponential distribution.</param>
-        /// <param name="timeUnit">The unit of the TimeSpan to return.</param>
-        /// <returns>The sampled time span in the specified unit.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean time span is less than or equal to zero or if an invalid unit is provided..</exception>
-        public static TimeSpan Sample(Random rs, TimeSpan mean, TimeUnit timeUnit)
-        {
-            return timeUnit switch
-            {
-                TimeUnit.Days => TimeSpan.FromDays(Sample(rs, mean.TotalDays)),
-                TimeUnit.Hours => TimeSpan.FromHours(Sample(rs, mean.TotalHours)),
-                TimeUnit.Minutes => TimeSpan.FromMinutes(Sample(rs, mean.TotalMinutes)),
-                TimeUnit.Seconds => TimeSpan.FromSeconds(Sample(rs, mean.TotalSeconds)),
-                TimeUnit.Milliseconds => TimeSpan.FromMilliseconds(Sample(rs, mean.TotalMilliseconds)),
-                _ => throw new ArgumentOutOfRangeException(nameof(timeUnit), "Unsupported time unit.")
-            };
-        }
+        return MathNet.Numerics.Distributions.Exponential.CDF(1 / mean, x);
+    }
 
-        /// <summary>
-        /// Computes the cumulative distribution function (CDF) for a given value.
-        /// </summary>
-        /// <param name="mean">The mean of the exponential distribution.</param>
-        /// <param name="x">The value for which the CDF is computed.</param>
-        /// <returns>The CDF value.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean is less than or equal to zero.</exception>
-        public static double CDF(double mean, double x)
-        {
-            ValidateMean(mean);
+    /// <summary>
+    /// Computes the inverse cumulative distribution function (InvCDF) for a given probability.
+    /// </summary>
+    /// <param name="mean">The mean of the exponential distribution.</param>
+    /// <param name="p">The probability for which the InvCDF is computed.</param>
+    /// <returns>The value corresponding to the given probability.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean is less than or equal to zero.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the probability is not between 0 and 1.</exception>
+    public static double InvCdf(double mean, double p)
+    {
+        ValidateMean(mean);
+        if (p is < 0 or > 1) throw new ArgumentOutOfRangeException(nameof(p), "Probability must be between 0 and 1.");
 
-            return MathNet.Numerics.Distributions.Exponential.CDF(1 / mean, x);
-        }
+        return MathNet.Numerics.Distributions.Exponential.InvCDF(1 / mean, p);
+    }
 
-        /// <summary>
-        /// Computes the inverse cumulative distribution function (InvCDF) for a given probability.
-        /// </summary>
-        /// <param name="mean">The mean of the exponential distribution.</param>
-        /// <param name="p">The probability for which the InvCDF is computed.</param>
-        /// <returns>The value corresponding to the given probability.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean is less than or equal to zero.</exception>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when the probability is not between 0 and 1.</exception>
-        public static double InvCDF(double mean, double p)
-        {
-            ValidateMean(mean);
-            if (p < 0 || p > 1) throw new ArgumentOutOfRangeException(nameof(p), "Probability must be between 0 and 1.");
+    /// <summary>
+    /// Validates that the mean is greater than zero.
+    /// </summary>
+    /// <param name="mean">The mean value to validate.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean is less than or equal to zero.</exception>
+    private static void ValidateMean(double mean)
+    {
+        const double epsilon = 1e-10; // A small value to avoid division by zero
 
-            return MathNet.Numerics.Distributions.Exponential.InvCDF(1 / mean, p);
-        }
-
-        /// <summary>
-        /// Validates that the mean is greater than zero.
-        /// </summary>
-        /// <param name="mean">The mean value to validate.</param>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when the mean is less than or equal to zero.</exception>
-        private static void ValidateMean(double mean)
-        {
-            double epsilon = 1e-10; // A small value to avoid division by zero
-
-            if (mean <= epsilon) throw new ArgumentOutOfRangeException(nameof(mean), "Mean must be greater than zero.");
-        }
+        if (mean <= epsilon) throw new ArgumentOutOfRangeException(nameof(mean), "Mean must be greater than zero.");
     }
 }
