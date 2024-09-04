dc21e454ba27ce53306e2de3465d5975cacad2a4
goh-chunlin <chunlin@live.com.sg>
2024-09-04 10:56:40 +0800

    Refactored Server.

---
+++ O2DESNet/StaticConfigs/ServerStaticConfig.cs
@@ -0,0 +1,14 @@
+namespace O2DESNet.StaticConfigs;
+
+public class ServerStaticConfig<TLoad> : IStaticConfig
+{
+    /// <summary>
+    /// Maximum number of loads the server can handle at one time.
+    /// </summary>
+    public int Capacity { get; set; } = int.MaxValue;
+    
+    /// <summary>
+    /// Function that defines the service time for each load.
+    /// </summary>
+    public Func<TLoad, Random, TimeSpan> ServiceTime { get; set; }
+}
